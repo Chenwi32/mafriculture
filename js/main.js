3ce5e3291d31ee2958eb49570d4046b694b953b5
Chenwi <chenwieugene.j@gmail.com>
2021-06-14 18:06:36 +0100

    Added a funtion to slide up and down the cart when the cart icon is clicked

--- js/main.js
+++ js/main.js
@@ -1,55 +1,52 @@
+"use strict";
+
+///////////////////////////////////////////////////////////////////
+const cartDisplay = document.querySelector(".cartDisplay");
+const overlay = document.querySelector(".cart-overlay");
+const cartIcon = document.querySelector(".card1");
+///////////////////////////////////////////////////////////////////
+
 function mafriculture() {
+  ///////////////////////////// Toggle menu //////////////////////////////////
+  function toggle() {
+    const btn = document.getElementById("btn");
+    const menu = document.querySelector(".nav-menu");
 
+    btn.addEventListener("click", () => {
+      let value = menu.classList.contains("navbar-collapse");
 
-    /*-------Toggle menu------*/
+      // This checks if the navbar__collapse and change classes are added to
+      // the menu ul if not it should add
+      if (value) {
+        menu.classList.remove("navbar-collapse");
+        btn.classList.add("change");
+      } else {
+        menu.classList.add("navbar-collapse");
 
-    function toggle() {
-        const btn = document.getElementById('btn');
-        const menu = document.querySelector('.nav-menu');
+        btn.classList.remove("change");
+      }
+    });
+  }
 
-        btn.addEventListener('click', () => {
-            let value = menu.classList.contains('navbar-collapse');
+  toggle();
+  ///////////////////////////////////////////////////////////////////////////
 
+  ////////////////////// Slide down and hide cart /////////////////////////////////
 
-            // This checks if the navbar__collapse and change classes are added to 
-            // the menu ul if not it should add
-            if (value) {
-                menu.classList.remove('navbar-collapse');
-                btn.classList.add('change');
-            }
-            else {
-                menu.classList.add('navbar-collapse');
-
-                btn.classList.remove('change');
-            }
-        })
+  const slideCartDown = () => {
+    if (cartDisplay.classList.contains("hide")) {
+      cartDisplay.classList.remove("hide");
+      overlay.classList.remove("hide");
+    } else {
+      cartDisplay.classList.add("hide");
+      overlay.classList.add("hide");
     }
+  };
 
-    toggle();
+  cartIcon.addEventListener("click", slideCartDown);
 
-    function cartPage(){
-        const cartBtn = document.querySelectorAll('.cartBtn');
-        const displayCart = document.querySelectorAll('.cartDisplay');
-        cartBtn.forEach( button => {
-             button.addEventListener('click', () => {
-            let value = displayCart.classList.contains('hide');
-            if (value){
-                displayCart.classList.remove('hide');
-                displayCart.classList.add('show');
-            }else{
-                displayCart.classList.add('hide');
-            }
-        });
-
-        });
-    }
-
-    cartPage();
-
+  overlay.addEventListener("click", slideCartDown);
+  //////////////////////////////////////////////////////////////////////////////
 }
 
 mafriculture();
-
-
-
-
--- js/products.js
+++ js/products.js
@@ -1,15 +1,17 @@
-    const productDisplay = document.querySelector('.product-display');
- /*    const firstPage = document.getElementById('first-page');
-    const secondPage = document.getElementById('more-products');
-    const next = document.getElementById('next');
- */
+"use strict";
+
+//////////////////////////////////////////////////////////////////
+const productDisplay = document.querySelector(".product-display");
+
+//////////////////////////////////////////////////////////////////
 
 // UI
 class UI {
-    displayProducts(obj){
-        let results = '';
-        obj.forEach(({id, title, price, image}) => {
-            results +=`
+  ///////////// This displays products from the Json file //////////////
+  displayProducts(obj) {
+    let results = "";
+    obj.forEach(({ id, title, price, image }) => {
+      results += `
                 <div class="col-4 card" id="p4">
                 <a href="product-details.html"><img src=${image} alt=""/></a>
                 <h3>${title}</h3>
@@ -23,53 +25,45 @@ class UI {
                     <a href="" data-id=${id}>Add to cart</a>
                     <p>FCFA ${price}</p>
                 </div>`;
-            
-        });
-        productDisplay.innerHTML = results; 
-    }
-};
-
-// Storage
-class Storage{
-
+    });
+    productDisplay.innerHTML = results;
+  }
+  ////////////////////////////////////////////////////////////////////////////
 }
 
+// Storage
+class Storage {}
 
-// Products 
-class Products{
+// Products
+class Products {
+  // This will get the products from the JSON by fetching them
+  // Because we will be fetching data from an external source, it might be delaying or might encounter
+  // errors so we will be using asyncronous and try catch.
 
-    // This will get the products from the JSON by fetching them 
-    // Because we will be fetching data from an external source, it might be delaying or might encounter
-    // errors so we will be using asyncronous and try catch.
+  async getProducts() {
+    // Because we will be dealing with get data from external source, we need to use the
+    // try catch method so that it will catch any error that will arrise along the way like this;
 
+    try {
+      // This will go and fetch the data, assign the data to the recievedData variable
+      const results = await fetch("../js/products.json");
+      const recievedData = await results.json();
 
-    async getProducts(){
+      // This will extract the items object which is in the fetched data
+      const products = recievedData.items;
 
-        // Because we will be dealing with get data from external source, we need to use the
-        // try catch method so that it will catch any error that will arrise along the way like this;
-
-        try{
-
-            // This will go and fetch the data, assign the data to the recievedData variable
-            const results = await fetch("../js/products.json");
-            const recievedData = await results.json();
-
-           // This will extract the items object which is in the fetched data 
-            const products = recievedData.items;
-
-           // This will return the products to be used outside this function 
-            return products;
-
-        }catch(err){
-            console.log(err);
-        }
+      // This will return the products to be used outside this function
+      return products;
+    } catch (err) {
+      console.log(err);
     }
-};
-
+  }
+}
 
 // This will update the DOM after loading
-document.addEventListener('DOMContentLoaded', /* because the getProducts() is async, to call it here, we must also make this async like this; */ 
-async () => {
+document.addEventListener(
+  "DOMContentLoaded" /* because the getProducts() is async, to call it here, we must also make this async like this; */,
+  async () => {
     const ui = new UI();
     const products = new Products();
 
@@ -81,8 +75,5 @@ async () => {
     // by recieving the products through the productobj as its parameter
 
     ui.displayProducts(productsObj);
-});
-
-
-
-
+  }
+);
